14a8edc684c19b11389eff2db2521df7f37fd9a5
Arhip <arhipparubets@gmail.com>
2025-03-21 22:53:43 +0100

    Added city weather parameters

--- script.js
+++ script.js
@@ -1,16 +1,57 @@
-const link = 'http://api.weatherstack.com/current?access_key=1306b684b49ba61c1f8d4615068e5cb7'
+const link = 'http://api.weatherstack.com/current?access_key=d305dc688fd65e428eef0e4abf296ef6'
+const root = document.getElementById('root')
 
-const store = {
-    city: 'London',
+let store = {
+    city: "London", feelslike: 0,
+    cloudcover: 0,
+    temperature: 0,
+    humidity: 0,
+    observationTime: "00:00 AM",
+    pressure: 0,
+    uvIndex: 0,
+    visibility: 0,
+    isDay: "yes",
+    description: 0,
 }
 
 const fetchData = async () => {
     const result = await fetch(`${link}&query=${store.city}`)
     const data = await result.json()
-    const { current: { feelslike, cloudcover, temperature } } = data
 
-    console.log('data', data)
+    const { current: {
+        humidity,
+        feelslike,
+        cloudcover,
+        temperature,
+        observation_time: observationTime,
+        pressure,
+        uv_index: uvIndex,
+        visibility,
+        is_day: isDay,
+        weather_descriptions: description,
+        wind_speed: windSpeed,
+    } } = data
+
+    store = {
+        ...store,
+        isDay,
+        pressure,
+        uvIndex,
+        feelslike,
+        cloudcover,
+        temperature,
+        humidity,
+        observationTime,
+        visibility,
+        windSpeed,
+        description: description[0],
+    }
+
+    renderComponent()
+}
+
+const renderComponent = () => {
+    root.innerHTML = `${store.temperature}°`
 }
 
 fetchData()
-
